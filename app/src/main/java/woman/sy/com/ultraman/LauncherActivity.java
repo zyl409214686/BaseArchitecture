00aadcee9361ab0944e845b1cbf209d8b1c42a52
zouyulong <zouyulong@leho.com>
2017-03-16 15:03:56 +0800

    create project & add launcher activity

---
+++ app/src/main/java/woman/sy/com/ultraman/LauncherActivity.java
@@ -0,0 +1,46 @@
+package woman.sy.com.ultraman;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+
+public class LauncherActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final View view = View.inflate(this, R.layout.activity_launcher, null);
+        setContentView(view);
+        //渐变展示启动屏
+        AlphaAnimation aa = new AlphaAnimation(0.3f, 1.0f);
+        aa.setDuration(2000);
+        view.startAnimation(aa);
+        aa.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                redirectTo();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+        });
+    }
+
+    /**
+     * 跳转到...
+     */
+    private void redirectTo() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
